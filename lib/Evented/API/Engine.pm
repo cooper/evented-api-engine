5018167a298c80b727fa681a7e891c4ccd2e4e00
Mitchell Cooper <cooper879@msn.com>
2013-06-29 00:04:27 -0400

    0.2: more base of Engine

--- lib/Evented/API/Engine.pm
+++ lib/Evented/API/Engine.pm
@@ -7,18 +7,76 @@ use warnings;
 use strict;
 use 5.010;
 
+use Carp;
+
 use Evented::Object;
 use parent 'Evented::Object';
 
 use Evented::API::Module;
 use Evented::API::Hax;
 
-our $VERSION = '0.1';
+our $VERSION = '0.2';
 
 # create a new API Engine.
+#
+# Evented::API::Engine->new(
+#     mod_inc  => ['mod', '/usr/share/something/mod'],
+#     features => qw(io-async something-else),
+#     modules  => $conf->keys_for_block('modules')
+# );
+#
 sub new {
     my ($class, %opts) = @_;
-    return bless \%opts, $class;
+    
+    # Determine the include directories.
+    
+    my $inc;
+    
+    # mod_inc is present and an array reference.
+    if (defined $opts{mod_inc} && ref $opts{mod_inc} eq 'ARRAY') { $inc = $opts{mod_inc} }
+    
+    # mod_inc is present but not an array reference.
+    elsif (defined $opts{mod_inc}) { $inc = [ $opts{mod_inc} ] }
+    
+    # nothing; use default include directories.
+    else { $inc = ['.', 'mod'] }
+    
+    # create the API Engine.
+    my $api = bless {
+        mod_inc => $inc
+    }, $class;
+    
+    $api->_configure_api();
+    return $api;
+}
+
+# handles post-construct constructor arguments.
+#
+#    features   =>  automatic ->add_feature()s
+#    modules    =>  automatic ->load_module()s
+#
+sub _configure_api {
+    my ($api, %opts) = @_;
+    
+    # automatically add features.
+    if (defined $opts{features}) {
+        $api->add_feature($_) foreach @{
+            ref $opts{features} eq 'ARRAY' ?
+            $opts{features}                :
+            [ $opts{features} ]
+        };
+    }
+    
+    # automatically load modules.
+    if (defined $opts{modules}) {
+        $api->load_modules_initially(@{
+            ref $opts{modules} eq 'ARRAY' ?
+            $opts{modules}                :
+            [ $opts{modules} ]
+        });
+    }
+    
+    return 1;
 }
 
 #########################
@@ -26,11 +84,40 @@ sub new {
 #########################
 
 # load modules initially, i.e. from a configuration file.
+# returns the number of modules that loaded successfully.
 sub load_modules_initially {
+    my ($api, @mod_names) = @_;
+    $api->{load_block} = { in_block => 1 };
+    
+    # load each module from within a load block.
+    my @results;
+    push @results, $api->load_module($_) foreach @mod_names;
+    
+    delete $api->{load_block};
+    return scalar grep { $_ } @results;
 }
 
 # load a module.
 sub load_module {
+    my ($api, $mod_name) = @_;
+    
+    # we are in a load block.
+    if ($api->{load_block}) {
+
+        # make sure this module has not been attempted.
+        if ($api->{load_block}{$mod_name}) {
+            carp "Skipping '$mod_name' because it was already attempted.";
+            return;
+        }
+    
+        # add to attempted list.
+        $api->{load_block}{$mod_name} = 1;
+        
+    }
+    
+    # TODO: load the module.
+    
+    return 1;
 }
 
 # unload a module.
