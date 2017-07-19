ab59d73b79d6f89624720bc099c39343bffa4c5c
Mitchell Cooper <githubspam@rlygd.net>
2017-07-18 22:05:24 -0400

    4.10: move Pod::Select to configure_requires

--- lib/Evented/API/Engine.pm
+++ lib/Evented/API/Engine.pm
@@ -15,7 +15,7 @@ use Module::Loaded qw(mark_as_loaded mark_as_unloaded is_loaded);
 use Evented::Object;
 use parent 'Evented::Object';
 
-our $VERSION = '4.09';
+our $VERSION = '4.10';
 
 use Evented::API::Module;
 use Evented::API::Events;
